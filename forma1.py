cf53f24f34ece15539d2756af4cf9532757959ef
Hot Dominik <forro.dominik.adrian@diak.szbi-pg.hu>
2026-02-25 09:17:35 +0100

    Keeesz

--- forma1.py
+++ forma1.py
@@ -13,10 +13,67 @@ Olvasd be az f1.txt adatait, majd oldd meg az alábbi feladatokat!
 A megoldott feladatokat a kiirt_adatok nevű mappába hozd létre statisztika.txt néven!
 """
 
+direct = dict()
+lst = []
+with open("beolvasando_adatok/f1.txt","r",encoding="UTF-8") as forrasfajl:
+    next(forrasfajl)
+    for sor in forrasfajl:
+        elemek = sor.strip().split(";")
+        gyozelem = int(elemek[2])
+        futam = int(elemek[3])
+        direct = dict(nev = elemek[0],csapat = elemek[1], gyozelmek = gyozelem,futamok = futam)
+        lst.append(direct)
 
-print("1. A beolvasott fájlban összesen ____ versenyző szerepel.")
-print("2. A legtöbb futamot nyert versenyző: ____")
-print("3. A legkevesebb futamot nyert versenyző: ____")
-print("4. A legtöbb futamot teljesített versenyző: ____")
-print("5. Az átlagos futamszám: ____")
-print("***A legtöbb futamgyőzelmet szerző csapat: ____")
+def futamok(lst,melyik):
+    legtobb = 0
+    legt = ""
+    legkevesebb = 10000
+    legk = ""
+    for i in range(len(lst)):
+        if legtobb < lst[i]["gyozelmek"]:
+            legtobb = lst[i]["gyozelmek"]
+            legt = lst[i]["nev"]
+        if legkevesebb > lst[i]["gyozelmek"]:
+            legkevesebb = lst[i]["gyozelmek"]
+            legk = lst[i]["nev"]
+    if melyik == True:
+        return legt
+    if melyik == False:
+        return legk
+
+def futamsz(lst,melyik):
+    legtobb = 0
+    legt = ""
+    avrg = 0
+    for i in range(len(lst)):
+        if legtobb < lst[i]["futamok"]:
+            legtobb = lst[i]["futamok"]
+            legt = lst[i]["nev"]
+        avrg += lst[i]["futamok"]
+    if melyik == True:
+        return legt
+    else:
+        return round(avrg / len(lst), 2)
+
+
+def csapat(lst):
+    idk = []
+    thisdict = {}
+    for i in range(len(lst)):
+            gyozelmek = lst[i]["gyozelmek"]
+            if lst[i]["csapat"] in thisdict:
+                thisdict[lst[i]["csapat"]] += gyozelmek
+            else:
+                thisdict[lst[i]["csapat"]] = gyozelmek
+        
+    return max(thisdict, key=thisdict.get)
+
+
+
+print(lst)
+print(f"1. A beolvasott fájlban összesen {len(lst)} versenyző szerepel.")
+print(f"2. A legtöbb futamot nyert versenyző: {futamok(lst,True)}")
+print(f"3. A legkevesebb futamot nyert versenyző: {futamok(lst,False)}")
+print(f"4. A legtöbb futamot teljesített versenyző: {futamsz(lst,True)}")
+print(f"5. Az átlagos futamszám: {futamsz(lst,False)}")
+print(f"***A legtöbb futamgyőzelmet szerző csapat: {csapat(lst)}")
